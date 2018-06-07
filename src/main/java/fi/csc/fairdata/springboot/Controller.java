1764afeeb5e1ef2370f46260c568c4bc7ecb395f
jarve <pj@csc.fi>
2018-06-07 16:31:58 +0300

    tiedostokopiointi onnistui, projekti yhdistetään fairdata-downloadiin

--- src/main/java/fi/csc/fairdata/springboot/Controller.java
+++ src/main/java/fi/csc/fairdata/springboot/Controller.java
@@ -3,6 +3,12 @@
  */
 package fi.csc.fairdata.springboot;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,16 +18,30 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 
-@RestController //should be@Controller
+@RestController
 @RequestMapping("/api/v1/dataset")
 public class Controller {
 
 	
 	    @RequestMapping(value = "/{id}")
-	    public Dataset dataset(@PathVariable("id") String id,
+	    public void dataset(@PathVariable("id") String id,
 	    		@RequestParam(value="file", required = false) String file,
-	    		@RequestParam(value="dir", required = false) String dir) {
-	        return new Dataset(id, file, dir);
+	    		@RequestParam(value="dir", required = false) String dir,
+	    		HttpServletResponse response) {
+	    	Dataset ds = new Dataset(id, file, dir);
+	    	response.setContentType("application/octet-stream; charset=UTF-8");
+	    	try {
+	    	response.addHeader("Content-Disposition", "attachment; filename="+ds.getFile()
+	    	+ "\"; filename*=UTF-8''" +URLEncoder.encode(ds.getFile(), "UTF-8"));
+			} catch (UnsupportedEncodingException e) {
+				System.err.println("UTF-8 ei muka löydy!");
+				e.printStackTrace();
+			}
+	    	try {
+				ds.copy(response.getOutputStream());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 	    }
 	
 
--- src/main/java/fi/csc/fairdata/springboot/Dataset.java
+++ src/main/java/fi/csc/fairdata/springboot/Dataset.java
@@ -3,6 +3,15 @@
  */
 package fi.csc.fairdata.springboot;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.servlet.ServletOutputStream;
+
 /**
  * @author pj
  *
@@ -29,5 +38,39 @@ public class Dataset {
 	public String getDir() {
 		return dir;
 	}
-	
+
+	public void copy(ServletOutputStream outputStream) {
+
+		HttpURLConnection con = null;
+		BufferedOutputStream bof = new BufferedOutputStream(outputStream);
+		try {
+			URL url = new URL("http://www.nic.funet.fi/index/geodata/ilmatiede/wind_speed/Wind_10y_return_level.tif");
+			con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			BufferedInputStream in = new BufferedInputStream(con.getInputStream());
+			in.transferTo(bof);
+			bof.flush();
+			in.close();
+			con.disconnect(); // ??
+			bof.close();
+		} catch (IOException e2) {
+			try {
+				int respCode = ((HttpURLConnection) con).getResponseCode();
+				InputStream es = ((HttpURLConnection) con).getErrorStream();
+				int ret = 0;
+				byte[] buf = new byte[8192];
+				System.err.print("Ida virhetilanne " + respCode + ": ");
+				while ((ret = es.read(buf)) > 0) {
+					bof.write(buf);
+					System.err.write(buf);
+					System.err.println();
+				}
+				es.close();
+				// return new MetaxResponse(respCode, buf.toString());
+			} catch (IOException e3) {
+				System.err.println(e3.getMessage());
+			}
+			System.err.println(e2.getMessage());
+		}
+	}
 }
