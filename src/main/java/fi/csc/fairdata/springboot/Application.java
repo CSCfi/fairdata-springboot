b679858e6963084bcfbd06fb1e389a4bc736ca8c
jarve <pj@csc.fi>
2018-06-08 11:39:19 +0300

    kerta kiellon päälle, war

--- src/main/java/fi/csc/fairdata/springboot/Application.java
+++ src/main/java/fi/csc/fairdata/springboot/Application.java
@@ -5,13 +5,20 @@ package fi.csc.fairdata.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * @author pj
  *
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+	
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(Application.class);
+	    }
 
 	/**
 	 * @param args
